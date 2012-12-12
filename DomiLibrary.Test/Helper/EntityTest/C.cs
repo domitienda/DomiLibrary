28f31c0252e4b5e3c027b0b3b255d1c3ad0a2fef
Iván Oliver <ivan.oliver@domitienda.com>
2012-12-12 12:45:22 +0100

    update C

--- DomiLibrary.Test/Helper/EntityTest/C.cs
+++ DomiLibrary.Test/Helper/EntityTest/C.cs
@@ -1,9 +1,13 @@
-﻿namespace DomiLibrary.Test.Helper.EntityTest
+﻿using System;
+
+namespace DomiLibrary.Test.Helper.EntityTest
 {
     public class C
     {
         public string Property4 { get; set; }
 
         public int Property5 { get; set; }
+
+        public DateTime Property6 { get; set; }
     }
 }
