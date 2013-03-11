eccd2202d68a9f8aaff9a28ed00c6183fab5c351
ioliver85 <ioliver85@gmail.com>
2013-03-11 12:14:19 +0100

    CollectionHelper - Subtract Generic

--- DomiLibrary.Utility/Helper/CollectionHelper.cs
+++ DomiLibrary.Utility/Helper/CollectionHelper.cs
@@ -1,5 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Web.Configuration;
 
 namespace DomiLibrary.Utility.Helper
 {
@@ -50,6 +52,48 @@ namespace DomiLibrary.Utility.Helper
             ValidationHelper.NotNull(b, "El paramentro b es nulo");
 
             var result = new ArrayList(a);
+
+            foreach (var item in b)
+            {
+                result.Remove(item);
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Devuelve una nueva coleccion que contiene a - b.
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public static ICollection Subtract<T>(ICollection<T> a, ICollection<T> b)
+        {
+            ValidationHelper.NotNull(a, "El paramentro a es nulo");
+            ValidationHelper.NotNull(b, "El paramentro b es nulo");
+
+            var result = new List<T>();
+
+            foreach (var item in b)
+            {
+                result.Remove(item);
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Devuelve una nueva coleccion que contiene a - b.
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public static ICollection Subtract<T>(T[] a, T[] b)
+        {
+            ValidationHelper.NotNull(a, "El paramentro a es nulo");
+            ValidationHelper.NotNull(b, "El paramentro b es nulo");
+
+            var result = new List<T>();
 
             foreach (var item in b)
             {
